0fbc6b90054506e7bdcf413120f0fa3403aa011d
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2025-02-06 13:10:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (93.95%)

--- 0048-rotate-image/solution.cpp
+++ 0048-rotate-image/solution.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& mat) {
-    int m = mat.size();       
-    int arr[m][m];
-    for(int i=0; i<m; i++){
-        for(int j=0; j<m; j++){
-            arr[i][j]=mat[i][j];
-        }
-    }
+    void rotate(vector<vector<int>>& arr) {
+        int n = arr.size(); 
 
-    for(int i=0; i<m; i++){
-        for(int j=0; j<m; j++){
-            mat[j][m-i-1]=arr[i][j];
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                
+                int temp = arr[i][j];
+
+                arr[i][j] = arr[n - 1 - j][i];
+
+                arr[n - 1 - j][i] = arr[n - 1 - i][n - 1 - j];
+
+                arr[n - 1 - i][n - 1 - j] = arr[j][n - 1 - i];
+
+                arr[j][n - 1 - i] = temp;
+            }
         }
-    }
     }
 };
+
