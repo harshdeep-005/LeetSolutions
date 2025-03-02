05fc2da32fca0e43adeb1096bd631f0661417228
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2025-03-02 06:40:43 +0000

    Sync LeetCode submission Runtime - 66 ms (75.03%), Memory - 173.2 MB (68.61%)

---
+++ 0146-lru-cache/solution.cpp
@@ -0,0 +1,62 @@
+struct Node {
+    int key, value;
+    Node* prev;
+    Node* next;
+    Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+};
+
+class LRUCache {
+private:
+    int capacity;
+    unordered_map<int, Node*> cache;
+    Node *head, *tail;
+
+    void removeNode(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void addNodeToFront(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+public:
+    LRUCache(int cap) : capacity(cap) {
+        head = new Node(-1, -1); 
+        tail = new Node(-1, -1); 
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (cache.find(key) == cache.end()) return -1;
+        
+        Node* node = cache[key];
+        removeNode(node);
+        addNodeToFront(node);
+        
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            node->value = value;
+            removeNode(node);
+            addNodeToFront(node);
+        } else {
+            if (cache.size() == capacity) {
+                Node* lru = tail->prev;
+                cache.erase(lru->key);
+                removeNode(lru);
+                delete lru;
+            }
+            Node* newNode = new Node(key, value);
+            cache[key] = newNode;
+            addNodeToFront(newNode);
+        }
+    }
+};
