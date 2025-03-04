ad27642dbb2dbcb68fbce2f7f49681cf8ec5bdeb
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2025-03-04 10:54:05 +0000

    Sync LeetCode submission Runtime - 36 ms (52.82%), Memory - 61.5 MB (97.70%)

--- 0215-kth-largest-element-in-an-array/solution.cpp
+++ 0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,12 +1,33 @@
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end());
+//         unordered_map<int, int> x;
+//         priority_queue<int> heap;
+//         for(auto i:nums){
+//             x[i]++;
+//             if(x[i]==1)heap.push(i);
+//         }
+//         int ans;
+//         int i=0;
+//         while (i<k) {
+//             ans=heap.top();
+//             heap.pop();
+//             i++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
-        for(int i=0; i<nums.size(); i++)pq.push(nums[i]);
-        for(int i=0; i<k-1; i++){
-            cout<<pq.top()<<" ";
-            pq.pop();
+        priority_queue<int> maxHeap(nums.begin(), nums.end());  
+        while (--k > 0) {
+            maxHeap.pop();
         }
-        return pq.top();
+
+        return maxHeap.top();  
     }
 };
+
