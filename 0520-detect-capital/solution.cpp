b91b5498ab6e0c15d32c003835e1b8dca5a1e0fc
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2025-03-18 18:22:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (50.91%)

---
+++ 0520-detect-capital/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size()==1)return true;
+        int n=word.size();
+        if(word[0] >= 'A' && word[0] <= 'Z'){
+            if(word[1] >= 'A' && word[1] <= 'Z'){
+                for(int i=1; i<n; i++){
+                    if(word[i] >= 'a' && word[i] <= 'z')return false;
+                }
+                return true;
+            }
+            else{
+                for(int i=1; i<n; i++){
+                    if(word[i] >= 'A' && word[i] <= 'Z')return false;
+                }
+                return true;
+            }
+        }
+        if(word[0] >= 'a' && word[0] <= 'z'){
+            for(int i=1; i<n; i++){
+                    if(word[i] >= 'A' && word[i] <= 'Z')return false;
+                }
+                return true;
+        }
+        return false;
+    }
+};
