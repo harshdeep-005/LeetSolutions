daa9022286ae0bd9cb7be24a1ab388e972d1db61
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2026-06-11 14:45:40 +0000

    Sync LeetCode submission Runtime - 4 ms (20.53%), Memory - 63 MB (49.70%)

--- 0011-container-with-most-water/solution.cpp
+++ 0011-container-with-most-water/solution.cpp
@@ -1,38 +1,15 @@
-// class Solution {
-// public:
-//     int maxArea(vector<int>& arr) {
-        
-//         int max=0, ar=0, n=arr.size();
-//         for(int i=0; i<n-1; i++){
-//             for(int j=i+1; j<n; j++){
-//                 if(arr[i]>arr[j])
-//                     ar=arr[j]*(j-i);
-//                 else
-//                     ar=arr[i]*(j-i);
-//                 if(ar>max)
-//                     max=ar;
-//             }
-//         }
-//         return max;
-//     }
-// };
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int maxArea = 0;
-        int left = 0;
-        int right = height.size() - 1;
-
-        while (left < right) {
-            maxArea = max(maxArea, (right - left) * min(height[left], height[right]));
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
+    int maxArea(vector<int>& s) {
+        int l=0, r=s.size()-1,maxi=0;
+        while(l<r){
+            int cs=min(s[l],s[r]);
+            int curr=(r-l)*cs;
+            maxi=max(curr,maxi);
+            if(s[l]<s[r])l++;
+            else r--;
         }
+        return maxi;
 
-        return maxArea;        
     }
 };
