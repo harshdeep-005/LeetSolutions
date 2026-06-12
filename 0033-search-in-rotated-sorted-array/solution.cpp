2b40b3d799b5f8a21a4730ebf50698f77857f702
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2026-06-12 08:44:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.3 MB (36.81%)

--- 0033-search-in-rotated-sorted-array/solution.cpp
+++ 0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int x=nums.size(), i;
-        for (i = 0; i<x; i++){
-            if(target==nums[i])
-                return i;
+    int fo(int l, int r, vector<int>& s, int tar){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(s[mid]==tar)return mid;
+            else if(s[mid]<tar)l=mid+1;
+            else r=mid-1;
         }
         return -1;
+    }
+    int search(vector<int>& s, int tar) {
+        int n=s.size(),l=0,r=n-1;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(s[mid]>s[n-1])l=mid+1;
+            else r=mid;
+        }
+        int rot=l;
+        l=0,r=n-1;
+        int ans;
+        if(tar >= s[rot] && tar <= s[n-1]) ans= fo(rot,n-1,s,tar);
+        else ans =fo(0,rot-1,s,tar);
+
+        return ans;
     }
 };
