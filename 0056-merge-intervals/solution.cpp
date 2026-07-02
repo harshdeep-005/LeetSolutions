4e6413d1c87664000c1b98ddf2f0f737203b411b
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2026-07-02 11:47:58 +0000

    Sync LeetCode submission Runtime - 7 ms (38.02%), Memory - 24.7 MB (16.38%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-    void foo(int &i, vector<vector<int>>& arr, vector<vector<int>>& intervals) {
-        vector<int> mergedInterval(2);
-        mergedInterval[0] = intervals[i][0];
-        mergedInterval[1] = intervals[i][1];
-
-        while (i < intervals.size() - 1 && mergedInterval[1] >= intervals[i + 1][0]) {
-            mergedInterval[1] = max(mergedInterval[1], intervals[i + 1][1]);
-            i++;  
-        }
-        
-        arr.push_back(mergedInterval);
-    }
-
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> arr;
-        int n = intervals.size(), i = 0;
-
+        vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
-
-        while (i < n) {
-            foo(i, arr, intervals);
-            i++; 
+        for(auto inter: intervals){
+            if(ans.empty()||ans.back()[1]<inter[0]){ans.push_back(inter);}
+            else ans.back()[1]=max(inter[1], ans.back()[1]);
         }
-
-        return arr;
+        return ans;
     }
 };
-
