0dff42340e93f4b19c0148295133e963c41fd582
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2026-07-06 13:35:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (45.97%)

--- 0102-binary-tree-level-order-traversal/solution.cpp
+++ 0102-binary-tree-level-order-traversal/solution.cpp
@@ -11,48 +11,22 @@
  */
 class Solution {
 public:
-    // void foo(TreeNode* root,priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>& pq, int n){
-    //     if (root == nullptr) return; 
-    //     pq.push({n, root->val});
-    //     foo(root->left, pq,n+1);
-    //     foo(root->right, pq,n+1);
-    // }
-    // vector<vector<int>> levelOrder(TreeNode* root) {
-    //     vector<vector<int>> arr;
-    //     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    //     int n=0, m=0;
-    //     foo(root,pq,n);
-    //     while (!pq.empty()) {
-    //         vector<int>ar;
-    //         do{
-    //         ar.push_back(value);
-    //         auto [key, value] = pq.top();
-    //         pq.pop();
-    //         m=key
-    //         }while(key==m)
-    //         arr.push_back(ar);
-    //         ar.clear();
-    //         m=key
-    //     }
-
-    //     return {{3},{9,20},{7,15}};
-    // }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==NULL)return ans;
-        queue<TreeNode*>q;
+        vector<vector<int>> ans;
+        if(root==nullptr)return ans;
+        queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
-            int s=q.size();
-            vector<int>v;
-            for(int i=0;i<s;i++){
-                TreeNode *node=q.front();
+            int n=q.size();
+            vector<int>res;
+            while(n--){
+                TreeNode* node=q.front();
+                res.push_back(node->val);
                 q.pop();
-                if(node->left!=NULL)q.push(node->left);
-                if(node->right!=NULL)q.push(node->right);
-                v.push_back(node->val);
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
             }
-            ans.push_back(v);
+            ans.push_back(res);
         }
         return ans;
     }
