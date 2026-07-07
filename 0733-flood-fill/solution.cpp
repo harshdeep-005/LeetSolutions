a4c10d42b357c0c70c2974c6dcdba96f9008f7f7
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2026-07-07 14:56:29 +0000

    Sync LeetCode submission Runtime - 1 ms (20.43%), Memory - 18.3 MB (43.69%)

--- 0733-flood-fill/solution.cpp
+++ 0733-flood-fill/solution.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>>arr(image);
-        int bro=arr[sr][sc];
-        if(bro==color)return arr;
-        int m=image.size(), n=image[0].size();
-        arr[sr][sc]=color;
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        while(!q.empty()){
-            int a=q.front().first, b=q.front().second;
-            q.pop();
-            for(int i=0; i<4; i++){
-                int nsr=a+dx[i], nsc=b+dy[i];
-                if(nsr < 0 || nsc < 0 || nsr >= m || nsc >= n || arr[nsr][nsc] != bro)continue;
-                arr[nsr][nsc]=color;
-                q.push({nsr,nsc});
+        if(image[sr][sc]==color)return image;
+        stack<pair<int,int>> st;
+        int dr[4]={1,-1,0,0}, dc[4]={0,0,1,-1};
+        int col=image[sr][sc],m=image.size(),n=image[0].size();
+        st.push({sr,sc});
+        while(!st.empty()){
+            int r=st.top().first,c=st.top().second;
+            st.pop();
+            image[r][c]=color;
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i],nc=c+dc[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n&& image[nr][nc]==col )st.push({nr,nc});
             }
         }
-        return arr;
+        return image;
     }
 };
