a4d62af1948a7db7658d8d6c171e219ecd4d15e4
Harshdeep Singh <148210526+harshdeep-005@users.noreply.github.com>
2026-08-02 12:54:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.3 MB (53.54%)

--- 0344-reverse-string/solution.cpp
+++ 0344-reverse-string/solution.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int l=0, r=s.size()-1;
-        char temp;
-        while(l<r){
-            temp=s[l];
-            s[l]=s[r];
-            s[r]=temp;
-            l++;
-            r--;
+        int l=0,r=s.size()-1;
+        while(l<=r){
+            swap(s[l],s[r]);
+            l++,r--;
         }
     }
 };
